c1caa2a1279b357251ab1ec1775c176aa401a8f1
Tibor Fazekas <tibcso0322@gmail.com>
2022-10-12 14:22:31 +0200

    made responsive

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,9 @@
   const instagram = 'https://www.instagram.com/zolikomuves_/';
   const tiktok = 'https://www.tiktok.com/@komuves.zoli_?lang=en';
   const twitter = 'https://twitter.com/komuves_zoli';
+  const youtube = 'https://www.youtube.com/c/ZoliKomuves/videos';
+
+  $: iconWidth = 50;
 </script>
 
 <svelte:head>
@@ -11,12 +14,13 @@
 
 <div class="h-full w-full relative">
   <div
-    class="w-1/2 h-full absolute left-1/2 -translate-x-1/2 bg-[#ceecb4] flex flex-col items-center"
+    class="w-[70%] max-w-3xl h-full absolute left-1/2 -translate-x-1/2 bg-[#ceecb4] flex flex-col items-center"
   >
     <header class="w-full flex flex-col justify-center items-center">
-      <h4 class="font-bold">sziasztok sefa vagyok</h4>
+      <h4 class="font-bold my-10">sziasztok sefa vagyok</h4>
       <div
-        class="w-[25rem] aspect-square bg-black relative"
+        class="aspect-square bg-black relative"
+        style={`width: ${window.innerWidth * 0.6}px; max-width: 30rem;`}
         on:mouseenter={() => {
           hovering = true;
         }}
@@ -29,7 +33,7 @@
           draggable="false"
           src="https://yt3.ggpht.com/tKGoIFNLbtGjSBQBGGPqYfu8D3uIydk0TM-q1PoIv15kg2qG7nCTiiIMIcPo01WcEAXoKER3=s900-c-k-c0x00ffffff-no-rj"
           alt="sefa"
-          width="1200"
+          width={window.innerWidth * 0.6}
         />
         <div
           style={`visibility: ${
@@ -41,15 +45,23 @@
         >
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <img
+            width={iconWidth}
+            class="cursor-pointer"
+            src="https://www.svgrepo.com/show/28730/youtube.svg"
+            alt="Twitter"
+            on:click={() => window.open(youtube)}
+          />
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <img
             src="https://www.svgrepo.com/show/157806/instagram.svg"
             alt="Instagram"
             class="cursor-pointer"
-            width="30"
+            width={iconWidth}
             on:click={() => window.open(instagram)}
           />
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <img
-            width="30"
+            width={iconWidth}
             class="cursor-pointer"
             src="https://www.svgrepo.com/show/342294/tiktok.svg"
             alt="TikTok"
@@ -57,7 +69,7 @@
           />
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <img
-            width="30"
+            width={iconWidth}
             class="cursor-pointer"
             src="https://www.svgrepo.com/show/183608/twitter.svg"
             alt="Twitter"
@@ -66,7 +78,6 @@
         </div>
       </div>
     </header>
-    <span>Szeretek gitározni</span>
-    <span>szeretem a kólát</span>
+    <span>Hobbim a gitározás</span>
   </div>
 </div>
